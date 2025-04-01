24674f93646b72ea405314f0499996675af00877
James Nyamweya <nyamweyaj@gmail.com>
2025-04-02 02:20:25 +0300

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainNode = document.getElementById("main");
+if (mainNode) {
+  mainNode.remove();
+}
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Ruth is the champion";
+document.body.appendChild(newHeader);
